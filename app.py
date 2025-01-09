17ff0b08dd54e065807e6367ab5adfb837ba3ee0
Kuba Kmiotek <kuba.kmiotek@snowflake.com>
2025-01-10 00:18:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
-URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
-XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
+URL = "https://www.unibet.fr/sport/football/europe/top-football-europeen?filter=Top+Paris&subFilter=R%C3%A9sultat+du+match"
+XPATH = "//*[@class='eventcard-team']"
 TIMEOUT = 20
 
 st.title("Test Selenium")
@@ -30,12 +30,11 @@ try:
     WebDriverWait(driver, TIMEOUT).until(
         EC.visibility_of_element_located((By.XPATH, XPATH,))
     )
-
 except TimeoutException:
     st.warning("Timed out waiting for page to load")
     driver.quit()
 
 time.sleep(10)
-elements = driver.find_elements_by_xpath(XPATH)
+elements = driver.find_elements(By.XPATH, XPATH)
 st.write([el.text for el in elements])
 driver.quit()
